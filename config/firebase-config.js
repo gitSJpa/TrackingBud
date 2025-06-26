c1988fed7a991e1a6651459c4c58ecf4e1a9bca4
gitSJpa <jota.santos773@gmail.com>
2025-06-27 00:15:12 +0100

    Finakl
    Everything seems to be in order

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,22 +1,24 @@
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getFirestore } from "firebase/firestore";
+
+const {
+  firebase: {
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+  },
+} = Constants.manifest.extra;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDThq9q1IaOd8tqPenK419QcKcJpqA7orA",
-  authDomain: "trackingbud-fcad4.firebaseapp.com",
-  projectId: "trackingbud-fcad4",
-  storageBucket: "trackingbud-fcad4.firebasestorage.app",
-  messagingSenderId: "225152083975",
-  appId: "1:225152083975:web:3f6574572743176b3c22a2",
-  measurementId: "G-SEHKY9EWZ8",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-const db = getFirestore(app);
-
-export { app, auth, db };
+export const firebaseApp = initializeApp(firebaseConfig);
